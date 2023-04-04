c8617bf2db7af142fab1608fdd9e13dd983e471b
tushar-9015 <109728513+tushar-9015@users.noreply.github.com>
2023-04-04 22:41:01 +0530

    commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+    </div>
+  )
+}
+
+export default about;
---
+++ pages/events/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const events = () => {
+  return (
+    <div>
+        <h2>Events</h2>
+      <a href=''>
+        <h2>events in london</h2>
+        </a>
+        <a href=''>
+        <h2>events in sans francisco</h2>
+        </a>
+        <a href=''>
+        <h2>events in barcelona</h2>
+        </a>
+    </div>
+  )
+}
+
+export default events
